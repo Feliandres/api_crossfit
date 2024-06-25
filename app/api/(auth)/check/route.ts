3941ff66c33f8ba89cee6dc638a4a4fc17a7235a
Feliandres <85601974+Feliandres@users.noreply.github.com>
2024-06-24 23:34:33 -0500

    Crear ruta API para chequear la sesion

---
+++ app/api/(auth)/check/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { getUserSession } from "@/data/session";
+
+export async function GET(req: Request) {
+    try {
+        // Obtener y validar la sesión del usuario
+        const { token, user, error, status } = await getUserSession(req);
+
+        if (error) {
+            return NextResponse.json({ error }, { status });
+        }
+
+        return NextResponse.json({
+            succcess: "Valid session",
+            user,
+            token,
+        }, { status: 200 });
+
+    } catch (error) {
+        return NextResponse.json({ error: "Unexpected error"}, { status: 500 });
+    }
+}
