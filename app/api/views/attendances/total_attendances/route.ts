5ca60ebd8870def96e43bc55cf311f24745dcae1
Feliandres <85601974+Feliandres@users.noreply.github.com>
2024-07-02 23:20:59 -0500

    Creando schema y rutas API para todas las vistas de los reportes

---
+++ app/api/views/attendances/total_attendances/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { Role } from "@prisma/client";
+import { getSessionAndValidateRole } from "@/data/session";
+
+export async function GET(req: Request) {
+    try {
+        // Obtener y validar la sesión del usuario
+        const { user, error, status } = await getSessionAndValidateRole(req);
+
+        if (error) {
+            return NextResponse.json({ error }, { status });
+        }
+
+        // Verificación adicional para asegurar que user no es null o undefined
+        if (!user) {
+            return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
+        }
+
+        if (user.role === Role.ADMIN) {
+
+            const total_attendances = await prisma.totalAttendances.findMany({
+
+            })
+
+            return NextResponse.json({
+                success: "Pay info retrieved successfully",
+                total_attendance: {
+                    ...total_attendances,
+                },
+            }, { status: 200 });
+        }
+
+    } catch (error) {
+        return NextResponse.json({ error: "Unexpected error"}, { status: 500 });
+    }
+}
---
+++ app/api/views/members/active_members/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { Role } from "@prisma/client";
+import { getSessionAndValidateRole } from "@/data/session";
+
+export async function GET(req: Request) {
+    try {
+        // Obtener y validar la sesión del usuario
+        const { user, error, status } = await getSessionAndValidateRole(req);
+
+        if (error) {
+            return NextResponse.json({ error }, { status });
+        }
+
+        // Verificación adicional para asegurar que user no es null o undefined
+        if (!user) {
+            return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
+        }
+
+        if (user.role === Role.ADMIN) {
+
+            const active_memberships = await prisma.activeMemberships.findMany({
+
+            })
+
+            return NextResponse.json({
+                success: "Pay info retrieved successfully",
+                active_membership: {
+                    ...active_memberships,
+                },
+            }, { status: 200 });
+        }
+
+    } catch (error) {
+        return NextResponse.json({ error: "Unexpected error"}, { status: 500 });
+    }
+}
