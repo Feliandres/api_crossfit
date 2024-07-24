5cbe3572c159237bf84b483035704c511108a9e7
Feliandres <85601974+Feliandres@users.noreply.github.com>
2024-07-23 19:13:29 -0500

    Cambios en actualizar usuario

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -58,11 +58,6 @@ export async function PUT(req: Request, { params }: { params: { id: string } }) 
             return NextResponse.json({ error }, { status });
         }
 
-        // Obtener Id del usuario
-        const userId = user?.id;
-        const userEmail = user?.email;
-        const userPassword = user?.password;
-
         // Verificar el rol del usuario para acceder a la ruta
         if (!user || user.role !== Role.ADMIN) {
             return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
@@ -75,7 +70,7 @@ export async function PUT(req: Request, { params }: { params: { id: string } }) 
         const validatedUser = UpdateUserSchema.parse(await req.json());
 
         // Verificar si el correo electrónico ya está en uso y actualizar
-        let emailChanged = null;
+        let emailChanged = false;
         if (validatedUser.email) {
             const existingUser = await getUserByEmail(validatedUser.email);
 
@@ -83,8 +78,10 @@ export async function PUT(req: Request, { params }: { params: { id: string } }) 
                 return NextResponse.json({ error: "Email already in use" }, { status: 401 });
             }
 
-            if (userEmail !== validatedUser.email) {
-                // Actualiza emailVerified a false
+            if (user.email !== validatedUser.email) {
+                emailChanged = true;
+
+                // Actualiza emailVerified a null
                 await prisma.user.update({
                     where: { id: idUser },
                     data: { emailVerified: null },
@@ -93,17 +90,14 @@ export async function PUT(req: Request, { params }: { params: { id: string } }) 
                 // Generar y enviar token de verificación
                 const verificationToken = await generateVerificationToken(validatedUser.email);
                 await sendVerificationEmail(validatedUser.email, verificationToken.token);
-
-                emailChanged = null;
             }
         }
 
         // Verificar y hashear la nueva contraseña si se proporciona
         let hashedPassword: string | undefined;
         if (validatedUser.password) {
-        
             // Comparar la nueva contraseña con la actual
-            const isPasswordSame = userPassword && await bcrypt.compare(validatedUser.password, userPassword);
+            const isPasswordSame = user.password && await bcrypt.compare(validatedUser.password, user.password);
 
             if (isPasswordSame) {
                 return NextResponse.json({ error: "New password cannot be the same as the current password" }, { status: 400 });
@@ -113,22 +107,23 @@ export async function PUT(req: Request, { params }: { params: { id: string } }) 
             hashedPassword = await bcrypt.hash(validatedUser.password, 12);
         }
 
-
-
         // Actualizar los datos del usuario en la base de datos
         const updatedUser = await prisma.user.update({
             where: { id: idUser },
             data: {
+                ...validatedUser,
                 email: validatedUser.email ?? undefined, // Solo actualizar si el email ha cambiado
                 password: hashedPassword ?? undefined, // Solo actualizar si se ha proporcionado una nueva contraseña
-                ...validatedUser // Actualizar otros datos del usuario
             },
         });
 
-
         return NextResponse.json({
-            success: "User updated successfully. Please verify the new email address.",
-            user: updatedUser,
+            success: "User updated successfully.",
+            ...(emailChanged && { message: "Please verify the new email address." }),
+            user: {
+                ...updatedUser,
+                password: hashedPassword, // Incluye el hash de la contraseña en la respuesta
+            },
         }, { status: 200 });
 
     } catch (error) {
@@ -138,6 +133,7 @@ export async function PUT(req: Request, { params }: { params: { id: string } }) 
         return NextResponse.json({ error: "Unexpected error" }, { status: 500 });
     }
 }
+
 
 //
 
