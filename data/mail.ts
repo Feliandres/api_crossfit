025748d184de3ac5ade0c0b9d54e8c3cc38cc9e6
Feliandres <85601974+Feliandres@users.noreply.github.com>
2024-07-25 15:54:24 -0500

    Cambios en email para verificaciones con dominio

--- data/mail.ts
+++ data/mail.ts
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 // Función para enviar correo de verificación
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `http://localhost:3001/new-verification?token=${token}`;
+  const confirmLink = `https://crossfit-app-beta.vercel.app/new-verification?token=${token}`;
 
   try {
     const response = await resend.emails.send({
@@ -21,7 +21,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 
 // Función para enviar correo de reinicio de contraseña
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const resetLink = `http://localhost:3001/new-password?token=${token}`;
+  const resetLink = `https://crossfit-app-beta.vercel.app/new-password?token=${token}`;
 
   try {
     const response = await resend.emails.send({
